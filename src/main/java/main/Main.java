0d86ca1e341e0fcfa6d772616a6d310530ca5a16
xsu <xsu@thoughtworks.com>
2016-07-21 14:09:01 +0800

    Diamond

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,14 +8,22 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+            showOneLine(n, i);
         }
+
+        for (int i = n -1; i >= 1; i--) {
+            showOneLine(n, i);
+        }
+    }
+
+    private static void showOneLine(int n, int i) {
+        for (int j = 1; j <= n - i; j++) {
+            System.out.print(" ");
+        }
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
     }
 
 }
