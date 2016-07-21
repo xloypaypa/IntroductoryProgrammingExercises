a38478da459de7d397b9499834c4d21e0766e75b
xsu <xsu@thoughtworks.com>
2016-07-21 14:03:50 +0800

    Draw a horizontal line

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,15 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("*");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
     }
 
 }
