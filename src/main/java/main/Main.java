4e3f11e75da36440fc0089ec3b62cdb723bf452d
xsu <xsu@thoughtworks.com>
2016-07-21 14:18:25 +0800

    Diamond with Name

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,13 +7,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n - 1; i++) {
             showOneLine(n, i);
         }
 
-        for (int i = n -1; i >= 1; i--) {
+        showName("Xin", 2 * n - 1);
+
+        for (int i = n - 1; i >= 1; i--) {
             showOneLine(n, i);
         }
+    }
+
+    private static void showName(String name, int len) {
+        for (int i = 1; i <= (len - name.length()) / 2; i++) {
+            System.out.print(" ");
+        }
+        System.out.println(name);
     }
 
     private static void showOneLine(int n, int i) {
